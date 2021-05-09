f2d182f53e59b6df46146bbd145964820c246e50
Archerist <yunuscemberci@gmail.com>
2021-05-09 12:49:55 +0300

    fix indentation

--- src/akakceScrap.ts
+++ src/akakceScrap.ts
@@ -10,31 +10,31 @@ const rx5600xtURL = 'https://www.akakce.com/ekran-karti,1,2.html/150008'
 
 export async function getAkakcePrice(gpu:string):Promise<any>{
 	let price;
-    let res;
-    const options:OptionsOfTextResponseBody = {
-        cache:false,
-        
-    }
-    switch(gpu){
-        case 'rx550':
-            res = await got(rx550URL,options)
-            break;
-        case 'rx560':
-            res = await got(rx560URL,options)
-            break;
-        case 'rx5500xt':
-            res = await got(rx5500xtURL, options)
-            break;
-        case 'rx5600xt':
-            res = await got(rx5600xtURL, options)
-            break;
-    
-    }
+	let res;
+	const options:OptionsOfTextResponseBody = {
+		cache:false,
+		
+	}
+	switch(gpu){
+		case 'rx550':
+			res = await got(rx550URL,options)
+			break;
+		case 'rx560':
+			res = await got(rx560URL,options)
+			break;
+		case 'rx5500xt':
+			res = await got(rx5500xtURL, options)
+			break;
+		case 'rx5600xt':
+			res = await got(rx5600xtURL, options)
+			break;
 	
-    let dom = new JSDOM(res.body);
+	}
+	
+	let dom = new JSDOM(res.body);
 	price = dom.window.document.querySelector('#CPL').firstElementChild.getElementsByClassName('pt_v8')[0].innerHTML;
-    price = parseInt(price.substring(0, price.indexOf('<')).replace('.',''))
+	price = parseInt(price.substring(0, price.indexOf('<')).replace('.',''))
 
-    return price;
+	return price;
 
 }
